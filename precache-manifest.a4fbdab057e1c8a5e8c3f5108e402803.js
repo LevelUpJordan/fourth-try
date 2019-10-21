e72e79d54c429f6c60e6393348f2c57c053ba382
Jordan <56843687+LevelUpJordan@users.noreply.github.com>
2019-10-21 20:55:00 +0100

    Updates

---
+++ precache-manifest.a4fbdab057e1c8a5e8c3f5108e402803.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4b00f0a91d240f610e3bd04b9ca325",
+    "url": "/fourth-try/index.html"
+  },
+  {
+    "revision": "eddbd777d6f0ec2dbfec",
+    "url": "/fourth-try/static/css/main.6024ca52.chunk.css"
+  },
+  {
+    "revision": "174e4038292f1f710e4e",
+    "url": "/fourth-try/static/js/2.94718f50.chunk.js"
+  },
+  {
+    "revision": "eddbd777d6f0ec2dbfec",
+    "url": "/fourth-try/static/js/main.a6afe165.chunk.js"
+  },
+  {
+    "revision": "f5b0f874e1a666bb82e1",
+    "url": "/fourth-try/static/js/runtime-main.52fbb339.js"
+  }
+]);
